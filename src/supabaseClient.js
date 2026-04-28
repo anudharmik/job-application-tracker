13515b97fee50bdf89302a87c65fd15b57f82695
anudharmik <24dharmika@rbunagpur.in>
2026-04-28 19:06:49 +0530

    solve env bug

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://nxtcrjszmxqyaixpthpb.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im54dGNyanN6bXhxeWFpeHB0aHBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzODUyMjksImV4cCI6MjA4Nzk2MTIyOX0.8w_Ad4vAE52xhdmDdfDdDYILDvVU5f_B5MxamGzwwFw";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
